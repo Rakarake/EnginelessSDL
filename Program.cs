e2604f14ca36bfd0968e69605e6e6bcb18af56f9
Rakarake <rak@rakarake.xyz>
2024-03-15 17:57:44 +0100

    Shooting bullets, updated README

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Engineless;
 using Engineless.Utils;
 using EnginelessSDL;
 
-//void ShrinkEverything(Res<Time> t, Query<Transform2D> q) {
-//    foreach (var h in q.hits) {
-//        h.Value.scaleX -= 0.05 * t.hit.delta;
-//        h.Value.scaleY -= 0.05 * t.hit.delta;
-//    }
-//}
-
-void PlayerMovement(Res<Time> time, Res<Input> i, Query<(Transform2D, Player)> q) {
+void PlayerMovement(IECS ecs, Res<Time> time, Res<Input> i, Query<(Transform2D, Player)> q) {
     var delta = time.hit.delta;
     foreach (var t in q.hits) {
         foreach (var k in i.hit.keysPressed) {
@@ -35,48 +27,42 @@ void PlayerMovement(Res<Time> time, Res<Input> i, Query<(Transform2D, Player)> q
         // Move the player
         t.Value.Item1.x += System.Math.Sin(t.Value.Item1.rotation) * delta * 300;
         t.Value.Item1.y += System.Math.Cos(t.Value.Item1.rotation) * delta * 300;
+
+        ecs.AddEntity(new List<Object> {
+            new Bullet(),
+            new Square(10, 10),
+            new Color() { r = 20, g = 20, b = 20, a = 20 },
+            new Transform2D() { x = t.Value.Item1.x, y = t.Value.Item1.y, rotation = t.Value.Item1.rotation },
+        });
     }
 }
 
-//void Shoot(Res<Intput> i, Query<(Transform2D, Player)> q) {
-//    foreach (var h in q.hits) {
-//        h.Value.
-//    }
-//}
+void MoveBullet(Res<Time> time, Query<(Transform2D, Bullet)> q) {
+    foreach (var t in q.hits) {
+        t.Value.Item1.x += System.Math.Sin(t.Value.Item1.rotation) * time.hit.delta * 300;
+        t.Value.Item1.y += System.Math.Cos(t.Value.Item1.rotation) * time.hit.delta * 300;
+    }
+}
+
 
 Engine ecs = new();
-//ecs.AddEntity(new List<Object>() {
-//    new Transform2D() { x = 3, y = 3, scaleX = 1, scaleY = 1 },
-//    new Square(400, 400),
-//    new Sprite("splooty.png"),
-//});
-//
-//ecs.AddEntity(new List<Object>() {
-//    new Transform2D() { x = 8, y = 8, scaleX = 1, scaleY = 1 },
-//    new Square(400, 400),
-//    new Color() { r = 80, g = 200, b = 150, a = 255 },
-//});
-
 // Player
 ecs.AddEntity(new List<Object>() {
     new Player(),
-    new Transform2D() { x = 40, y = 40, scaleX = 0.1, scaleY = 0.1 },
+    new Transform2D() { x = 40, y = 40 },
     new Sprite("splooty.png"),
-});
-ecs.AddEntity(new List<Object>() {
-    new Bullet(),
-    new Square(10, 10),
-    new Transform2D() { x = 20, y = 20 },
-    new Color() { r = 20, g = 20, b = 20, a = 20 },
 });
 
 ecs.SetResource(new SDLState() { windowWidth = 1200, windowHeight = 800, });
 ecs.AddSystem(Event.Startup, EnginelessSDL.EnginelessSDL.Initialize);
-//ecs.AddSystem(Event.Update, MoveAllSprites);
-//ecs.AddSystem(Event.Update, ShrinkEverything);
 ecs.AddSystem(Event.Update, PlayerMovement);
+ecs.AddSystem(Event.Update, MoveBullet);
 ecs.Start();
 
-class Player {}
+
+class Player {
+    double momentum = 0;
+}
+
 class Bullet {}
 
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Basic rendering using SDL for Engineless
-Very cool.
 
-```cs
+An incomplete graphics layer built on top of SDL2.
 
-```
+`Program.cs` is an example of how to use this module.
+
+TODO:
+* Rotation and scale needs to be properly applied to graphic objects
 
